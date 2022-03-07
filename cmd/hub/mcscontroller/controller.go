955bcdd3ce792186511bcbcb5cc7fdfb0e9d4836
Etai Lev Ran <etail@il.ibm.com>
2022-03-07 15:58:30 +0200

    reconcilation loop logic simplified
    Signed-off-by: Etai Lev Ran <etail@il.ibm.com>

--- cmd/hub/mcscontroller/controller.go
+++ cmd/hub/mcscontroller/controller.go
@@ -24,11 +24,12 @@ import (
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	mcsv1a1 "sigs.k8s.io/mcs-api/pkg/apis/v1alpha1"
+
+	"github.com/submariner-io/lighthouse/pkg/mcs"
 )
 
 const (
@@ -43,9 +44,9 @@ type ServiceExportReconciler struct {
 }
 
 // +kubebuilder:rbac:groups=multicluster.x-k8s.io,resources=serviceexports,verbs=get;list;watch;update;patch
-// +kubebuilder:rbac:groups=multicluster.x-k8s.io,resources=serviceexports/finalizers,verbs=get;update
+// +kubebuilder:rbac:groups=multicluster.x-k8s.io,resources=serviceexports/finalizers,verbs=get;update;delete
 // +kubebuilder:rbac:groups=multicluster.x-k8s.io,resources=serviceexports/status,verbs=get;update
-// +kubebuilder:rbac:groups=multicluster.x-k8s.io,resources=serviceimports,verbs=create;get;list;update;patch
+// +kubebuilder:rbac:groups=multicluster.x-k8s.io,resources=serviceimports,verbs=create;get;list;update;patch;delete
 
 // Reconcile handles an update to a ServiceExport
 // By design, the event that triggered reconciliation is not passed to the reconciler
@@ -55,8 +56,9 @@ func (r *ServiceExportReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	// @todo extract the real (i.e., source) object name, namespace and cluster
 	log := r.Log.WithValues("serviceexport", req.NamespacedName)
+
+	// @todo extract and log the real (i.e., source) object name, namespace and cluster?
 	log.Info("reconciling ServiceExport")
 
 	se := mcsv1a1.ServiceExport{}
@@ -69,59 +71,94 @@ func (r *ServiceExportReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 		return ctrl.Result{}, nil
 	}
 
+	result, err := r.reconcile(ctx, &se)
+	if err != nil {
+		return result, err
+	}
+
 	if se.GetDeletionTimestamp() != nil { // service export is marked to be deleted
 		return r.handleDelete(ctx, &se)
 	}
 
-	return r.handleUpdate(ctx, &se)
+	return result, err
 }
 
-// handle create and update of service export
-func (r *ServiceExportReconciler) handleUpdate(ctx context.Context, se *mcsv1a1.ServiceExport) (ctrl.Result, error) {
-	// @todo extract the real (i.e., source) object name, namespace and cluster
-	log := r.Log.WithValues("serviceexport", types.NamespacedName{se.Namespace, se.Name})
+// reconcile the cluster state.
+//
+// Reconciliation logic:
+// 1. list all relevant exports (i.e., having the same name and namespace labels)
+// 2. determine the primary export object
+// 3. iterate on all relevant export objects
+// 3a. if the object is being deleted (non empty deletion timestamp) - ignore
+// 3b. if object conflicts with primary - update export.Status.Condition.Conflict
+//		and delete corresponding ServiceImport.
+// 3c. otherwise, create/update the corrresponding ServiceImport and set the
+//		ServiceExport as owner.
+//
+// Notes:
+// - ensure objects are protected with a finalizer. Since we only receive the
+//	 affected object name into the reconciliation loop, we have no way to determine
+//	 and retrieve affected ServiceExports (i.e., client.List would not return the
+//   deleted object and we can't access its labels). The finalizer is cleared when we
+// 	 receive a notification for change in an object scheduled for deletion.
+// - ServiceImports are owned by the corresponding ServiceExport and are thus
+//	 automatically deleted by k8s when their ServiceExport is deleted.
+func (r *ServiceExportReconciler) reconcile(ctx context.Context, se *mcsv1a1.ServiceExport) (ctrl.Result, error) {
+	lh := mcs.Lighthouse{}
+	name := lh.GetOriginalObjectName(se.ObjectMeta)
+	log := r.Log.WithValues("serviceexport", name).WithValues("cluster", lh.GetOriginalObjectCluster(se.ObjectMeta))
 
-	if !controllerutil.ContainsFinalizer(se, serviceExportFinalizer) {
+	if !controllerutil.ContainsFinalizer(se, serviceExportFinalizer) && se.GetDeletionTimestamp() == nil {
 		controllerutil.AddFinalizer(se, serviceExportFinalizer)
 		if err := r.Client.Update(ctx, se); err != nil {
+			// @todo handle version mismatch error?
 			log.Error(err, "error adding finalizer")
 			return ctrl.Result{}, err
 		}
 	}
 
-	// @todo
-	// fetch all relevant ServiceExports
-	// Compute primary and call updateImports with new primary
-	// @todo avoid recomputing conflicts and service imports if primary hasn't changed
-	//   (marked by annotation or label). In that case only call compute single ServiceImport
+	//------------------------------------------
+	// -- @TODO CODE ABOVE SPECIFIED BEHAVIOR --
+	//------------------------------------------
+
 	return ctrl.Result{}, nil
 }
 
+// allow the object to be deleted by removing the finalizer.
+// Once all finalizers have been removed, the ServiceExport object and its
+// owned ServiceImport object will be deleted.
+//
+// Note: ownership will need to be reconsidered if merging imports
 func (r *ServiceExportReconciler) handleDelete(ctx context.Context, se *mcsv1a1.ServiceExport) (ctrl.Result, error) {
 	if controllerutil.ContainsFinalizer(se, serviceExportFinalizer) {
-		// @todo extract the real (i.e., source) object name, namespace and cluster
-		log := r.Log.WithValues("serviceexport", types.NamespacedName{se.Namespace, se.Name})
+		lh := mcs.Lighthouse{}
+		log := r.Log.WithValues("serviceexport", lh.GetOriginalObjectName(se.ObjectMeta)).
+			WithValues("cluster", lh.GetOriginalObjectCluster(se.ObjectMeta))
+
 		log.Info("removing service export")
 
-		// @todo:
-		// fetch all relevant service exports
-		// if removing the "primary" SE, recompute primary and call updateImports with new primary
-		// @todo: avoid recomputing conflicts if the spec of the previous and new primaries is identical?
-
-		// clear the finalizer (once all finalizers have been removed, the ServiceExport
-		// object and its owned ServiceImport object will be deleted).
-		// Note: ownership will need to be reconsidered if merging imports
+		// clear the finalizer
 		controllerutil.RemoveFinalizer(se, serviceExportFinalizer)
 		if err := r.Client.Update(ctx, se); err != nil {
+			// @todo handle version mismatch error?
 			return ctrl.Result{}, err
 		}
 	}
 	return ctrl.Result{}, nil
 }
 
+// SetupWithManager registers the reconciler with the manager.
 func (r *ServiceExportReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&mcsv1a1.ServiceExport{}).
 		Owns(&mcsv1a1.ServiceImport{}).
 		Complete(r)
+}
+
+// create or update the ServiceImport corresponding to the provided export.
+func (r *ServiceExportReconciler) ensureImportFor(se *mcsv1a1.ServiceExport) error {
+	//---------------------
+	//-- @TODO IMPLEMENT --
+	//---------------------
+	return nil
 }
